6eeb03007397343f2d90d4cafd0f394185ebb92e
Guy <king.guy@outlook.com>
2024-09-11 15:57:38 -0600

    again

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,6 @@
 # Install MKVToolNix
-sudo apt-get update
-sudo apt-get install -y mkvtoolnix
+apt-get update
+apt-get install -y mkvtoolnix
+apt-get install -y ffmpeg
 
 gunicorn --chdir app main:app
